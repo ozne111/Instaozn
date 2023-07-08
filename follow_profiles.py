ef5735e58a2562e2b54375ab21178a397405b63f
ozne111 <139002023+ozne111@users.noreply.github.com>
2023-07-08 14:19:18 -0300

    Update follow_profiles.py

--- follow_profiles.py
+++ follow_profiles.py
@@ -3,17 +3,14 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # Configuração do driver do Selenium
-driver_path = "/data/data/com.termux/files/usr/bin/chromedriver"
+driver_path = "caminho_para_o_seu_driver"  # Substitua pelo caminho para o seu driver
 
-# URL da página de login do Instagram
-url = "https://www.instagram.com/accounts/login/"
+# Informações de login do Instagram
+username = "seu_nome_de_usuário"  # Insira seu nome de usuário do Instagram
+password = "sua_senha"  # Insira sua senha do Instagram
 
-# Informações de login
-username = "seu_nome_de_usuário"
-password = "sua_senha"
-
-# Perfis a seguir e parar de seguir
-perfis_seguir = ["neymarjr", "leomessi"]
+# Perfis a seguir
+perfis_seguir = ["neymarjr", "selenagomez"]
 
 # Função para seguir um perfil
 def seguir_perfil(perfil):
@@ -23,15 +20,15 @@ def seguir_perfil(perfil):
     seguir_button.click()
     time.sleep(5)
 
-# Função para parar de seguir um perfil
-def parar_de_seguir_perfil(perfil):
-    driver.get("https://www.instagram.com/" + perfil)
+# Função para fazer login no Instagram
+def fazer_login(username, password):
+    driver.get("https://www.instagram.com/accounts/login/")
     time.sleep(2)
-    seguir_button = driver.find_element_by_xpath("//button[contains(text(), 'Seguindo')]")
-    seguir_button.click()
-    time.sleep(2)
-    confirmar_button = driver.find_element_by_xpath("//button[contains(text(), 'Deixar de seguir')]")
-    confirmar_button.click()
+    username_input = driver.find_element_by_name("username")
+    username_input.send_keys(username)
+    password_input = driver.find_element_by_name("password")
+    password_input.send_keys(password)
+    password_input.send_keys(Keys.ENTER)
     time.sleep(5)
 
 # Configuração do driver do Chrome
@@ -43,24 +40,13 @@ options.add_argument("--headless")  # Executar em modo headless, sem exibição 
 # Inicializar o driver do Chrome
 driver = webdriver.Chrome(executable_path=driver_path, options=options)
 
-# Fazer login
-driver.get(url)
-time.sleep(2)
-username_input = driver.find_element_by_name("username")
-username_input.send_keys(username)
-password_input = driver.find_element_by_name("password")
-password_input.send_keys(password)
-password_input.send_keys(Keys.ENTER)
-time.sleep(5)
+# Fazer login no Instagram
+fazer_login(username, password)
 
-# Seguir e parar de seguir perfis
-while True:
-    for perfil in perfis_seguir:
-        seguir_perfil(perfil)
-        time.sleep(5)
-    for perfil in perfis_seguir:
-        parar_de_seguir_perfil(perfil)
-        time.sleep(5)
+# Seguir perfis
+for perfil in perfis_seguir:
+    seguir_perfil(perfil)
+    time.sleep(5)
 
 # Fechar o driver
 driver.quit()
